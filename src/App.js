14e3d8948ff8181df01c9ff4b680fb2c64e01b10
amit-devops <amit26pandey@gmail.com>
2019-09-18 12:39:05 +0530

    update names for testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi - welcome to kelly - amit-arun-venkat! 
+         Hi - welcome to kelly - amit-arun-venkat-kiran-pratyu! 
         </p>
         <a
           className="App-link"
